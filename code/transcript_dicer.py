f0585f5cf0d596bf51a43909bea36a6dc6eecfcc
whb21 <45904718+whb21@users.noreply.github.com>
2019-01-08 14:54:47 -0500

    Add files via upload

---
+++ code/transcript_dicer.py
@@ -0,0 +1,17 @@
+#transcript_dicer.py
+#Will Baxley, 1/07/8
+#Context Lab, Dartmouth College, Hanover, NH
+
+# reads "fourforcestrans" and outputs
+
+infile = open("fourforcestrans", "r")
+lines = infile.read().split("\n")
+n = len(lines)/2        # number of lines with text
+i = 1                   # current start of the window
+while i + 14 <= n:
+    cluster = ""
+    for int in range(15):
+        cluster += lines[2 * (i + int) - 1] + " "
+    print(cluster + "\n")
+    i += 1          # shift the window over by 1
+infile.close()
