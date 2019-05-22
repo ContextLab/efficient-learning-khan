ca9b74e4f3d639eec6590eb330d1ef9d40800f64
Max Bluestone <mbluestone93@gmail.com>
2019-05-22 13:41:34 -0400

    add both functions for testing

--- code/data analysis/model_topics.py
+++ code/data analysis/model_topics.py
@@ -14,7 +14,7 @@ import numpy as np
 import os
 
 
-def model_topics(questions,videos,corpus=None,lda_params=None,vec_params=None):
+def model_topics(v_windows,q_windows,corpus=None,lda_params=None,vec_params=None):
     """
     Function that fits topic models to video content and uses it to transform video and question content
     
@@ -30,7 +30,76 @@ def model_topics(questions,videos,corpus=None,lda_params=None,vec_params=None):
         vec_params - 
         
     Output:
+        video_topics - topic model for the video
+        
+        questions_topics - topic model for the questions
+    """
+    print("Running model")
+    corpus = v_windows+q_windows
+    # initialize count vectorizer
+    tf_vectorizer = CountVectorizer(**vec_params)
+    # fit the model
+    tf_vectorizer.fit(v_windows+q_windows)
+
+    # transform video windows
+    video_tf = tf_vectorizer.transform(v_windows)
+
+    # transform question samples
+    questions_tf = tf_vectorizer.transform(q_windows)
+
+    both_tf = tf_vectorizer.transform(v_windows+q_windows)
+
+    # initialize topic model
+    lda = LatentDirichletAllocation(**lda_params)
+
+    # fit the topic model
+    lda.fit(both_tf)
+
+    # transform video topics
+    video_topics = lda.transform(video_tf)
+
+    # smooth model
+    video_topics = pd.DataFrame(video_topics).rolling(25).mean().loc[25:,:].as_matrix()
+
+    # transform question topics
+    questions_topics = lda.transform(questions_tf)
+    
+    return video_topics, questions_topics
+
+
+t1,t2 = model_topics(forces_video_samples,forces_questions_samples,lda_params=lda_params,vec_params=vec_params)
+
+def model_lessons(v_windows, q_windows, vec_params, lda_params):
+    """
+    Function that fits a topic model to video content and uses it to transform video and question content
     """
     
+    # initialize count vectorizer
+    tf_vectorizer = CountVectorizer(**vec_params)
+    # fit the model
+    tf_vectorizer.fit(v_windows+q_windows)
+
+    # transform video windows
+    video_tf = tf_vectorizer.transform(v_windows)
+
+    # transform question samples
+    questions_tf = tf_vectorizer.transform(q_windows)
+
+    both_tf = tf_vectorizer.transform(v_windows+q_windows)
+
+    # initialize topic model
+    lda = LatentDirichletAllocation(**lda_params)
+
+    # fit the topic model
+    lda.fit(both_tf)
+
+    # transform video topics
+    video_topics = lda.transform(video_tf)
+
+    # smooth model
+    video_topics = pd.DataFrame(video_topics).rolling(25).mean().loc[25:,:].as_matrix()
+
+    # transform question topics
+    questions_topics = lda.transform(questions_tf)
     
-    
+    return video_topics, questions_topics
