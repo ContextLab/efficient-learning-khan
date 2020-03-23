e053bb295ee5220609ca71b89aa2a1baff98665a
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-03-23 16:06:00 -0400

    moved files

--- code/khan_helpers/helpers.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.stats import entropy
-
-
-# symmetrized KL divergence
-# passed to scipy.spatial.distance.cdist in Participant.reconstruct_trace
-def symmetric_kl(a, b, c=1e-11):
-    return np.divide(entropy(a + c, b + c) + entropy(b + c, a + c), 2)
