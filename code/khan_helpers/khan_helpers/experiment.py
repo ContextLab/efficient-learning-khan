01339ae66173a7a56f6cb25ccf246b7e4ab1e7a6
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-03-23 16:04:30 -0400

    added named subdirectory

---
+++ code/khan_helpers/khan_helpers/experiment.py
@@ -0,0 +1,334 @@
+import re
+import hypertools as hyp
+import numpy as np
+import pandas as pd
+from datetime import timedelta
+from os.path import join as opj
+from warnings import warn
+from nltk.corpus import stopwords
+from PIL.Image import open as open_image
+
+
+DATADIR= '../../data'
+RAWDIR = opj(DATADIR, 'raw')
+PARTICIPANTS_DIR = opj(DATADIR, 'participants')
+TRAJS_DIR = opj(DATADIR, 'trajectories')
+EMBS_DIR = opj(DATADIR, 'embeddings')
+MODELS_DIR = opj(DATADIR, 'models')
+N_PARTICIPANTS = 50
+LECTURE_WSIZE = 15
+STOP_WORDS = stopwords.words('english') + ["even", "I'll", "I'm", "let", "let's",
+                                           "really", "they'd", "they're",
+                                           "they've", "they'll", "that's"]
+
+
+def _ts_to_sec(ts):
+    # converts timestamp of elapsed time
+    # from "MM:SS" format to scalar
+    mins, secs = ts.split(':')
+    mins, secs = int(mins), int(secs)
+    return timedelta(minutes=mins, seconds=secs).total_seconds()
+
+
+def format_text(textlist, sw=STOP_WORDS):
+    # some simple text preprocessing:
+    # removes punctuation & symbols, converts to all lowercase,
+    # splits hyphenated words, removes apostrophes
+    clean_textlist = []
+    for chunk in textlist:
+        no_punc = re.sub("[^a-zA-Z\s'-]+", '', chunk.lower()).replace('-', ' ')
+        no_stop = ' '.join([word for word in no_punc.split() if word not in sw])
+        clean_text = re.sub("'+", '', no_stop)
+        clean_textlist.append(clean_text)
+    return clean_textlist
+
+
+class Experiment:
+    def __init__(self):
+        self.participants = None
+        self.avg_participant = None
+        self.forces_transcript = None
+        self.bos_transcript = None
+        self.questions = None
+        self.forces_windows = None
+        self.bos_windows = None
+        self.forces_traj = None
+        self.bos_traj = None
+        self.question_vectors = None
+        self.answer_vectors = None
+        self.embedding_space = None
+        self.forces_embedding = None
+        self.bos_embedding = None
+        self.question_embeddings = None
+        self.wordle_mask = None
+        self.cv = None
+        self.cv_params = {
+            'strip_accents': 'unicode',
+            'stop_words': STOP_WORDS
+        }
+        self.lda = None
+        self.lda_params = {
+            'n_components': 25,
+            'learning_method': 'batch',
+            'random_state': 0
+        }
+        self.umap_reducer = None
+        self.umap_params = {
+            'n_components': 2,
+            'n_neighbors': 15,
+            'min_dist': 0.7,
+            'spread': 1.0,
+            'random_state': 0
+        }
+
+    def get_lecture_traj(self, lecture):
+        if hasattr(lecture, '__iter__') and not isinstance(lecture, str):
+            if len(lecture) > 1:
+                return [self.get_lecture_traj(l) for l in lecture]
+            else:
+                return self.get_lecture_traj(lecture[0])
+        elif lecture in ['forces', 1]:
+            return self.forces_traj
+        elif lecture in ['bos', 2]:
+            return self.bos_traj
+        else:
+            raise ValueError("Lecture should be either 1, 'forces', 2, or 'bos'")
+
+    def get_question_vecs(self, qids=None, lectures=None):
+        # get question topic vectors by question ID(s) or lecture(s)
+        if (qids is lectures is None) or (qids is not None and lectures is not None):
+            raise ValueError("must pass either qids or lecture (not both)")
+        if lectures is not None:
+            qids = []
+            if 'forces' in lectures:
+                qids += list(range(15))
+            if 'bos' in lectures:
+                qids += list(range(15, 30))
+            if 'general' in lectures:
+                qids += list(range(30, 39))
+        else:
+            qids = [qid - 1 for qid in qids]
+        return self.question_vectors[qids]
+
+    def get_timepoint_text(self, lecture, timepoint, buffer=15):
+        if lecture == 'forces':
+            transcript = self.forces_transcript
+        elif lecture == 'bos':
+            transcript = self.bos_transcript
+        else:
+            raise ValueError("Lecture must be either 'forces' or 'bos'")
+        # make sure necessary data is loaded
+        if transcript is None:
+            transcript = self.load_transcript(lecture)
+        # get timestamps and text from transcript
+        transcript = transcript.splitlines()
+        timestamps = np.fromiter(map(_ts_to_sec, transcript[::2]), dtype=np.int)
+        text = np.array(transcript[1::2])
+        # compute start and end time from timeoint and buffer
+        onset, offset = timepoint - buffer, timepoint + buffer
+        # make sure times are within bounds
+        if onset < 0:
+            onset = 0
+        if offset > timestamps[-1]:
+            offset = timestamps[-1]
+        # get timestamps of text between those times
+        text_ixs = np.where((timestamps >= onset) & (timestamps < offset))[0]
+        return ' '.join(text[text_ixs])
+
+    def plot(
+            self,
+            lectures=None,
+            questions=None,
+            participants=None,
+            keys=None,
+            **kwargs
+    ):
+        """
+        Wraps hypertools.plot for multi-subject plots and plotting
+        lectures/questions. Plotting order is:
+            1. lectures (if multiple, plotted in order passed)
+            2. questions (plotted in order passed)
+            3. traces (for each participant, in order passed, the trace given by
+                each key, in order passed)
+        :param lectures: str, int, or iterable of strs/ints (optional)
+                Lecture topic trajectories to plot
+        :param questions: str, int, or iterable of str/int (optional)
+                If str or iterable of strs, the category of questions ("forces",
+                "bos", "general") to plot. If int or iterable of ints, the IDs
+                of questions to plot
+        :param participants: str, int or iterable of strs/ints (optional)
+                The participant(s) in `self.participants` whose reconstructed
+                traces (given by keys arg) to plot.  May also be "all" for all
+                participants or "avg" for `self.avg_participant`
+        :param keys: str or iterable of str (required if passing `participants`)
+                The keys of reconstructed traces to plot for each participant
+        :param kwargs: various types
+                Keyword arguments passed to `hypertools.plot`, then forwarded to
+                matplotlib
+        :return: plot: hypertools.datageometry.DataGeometry
+                A plot of the specified data
+        """
+        if (participants is not None) and (keys is None):
+            raise ValueError("Must pass keys if passing participants")
+        # funnel args into iterables
+        if lectures is None:
+            lectures = []
+        elif isinstance(lectures, str):
+            lectures = [lectures]
+        if questions is None:
+            questions = []
+        elif isinstance(questions, (str, int)):
+            questions = [questions]
+        if participants is None:
+            participants = []
+            keys = []
+        elif isinstance(participants, str):
+            if participants == 'all':
+                participants = self.participants
+            elif participants == 'avg':
+                participants = [self.avg_participant]
+            else:
+                participants = [participants]
+        elif isinstance(participants, int):
+            participants = [f'P{participants}']
+        if isinstance(keys, str):
+            keys = [keys]
+        skip_keys = []
+        for key in keys:
+            if not ('forces' in key or 'bos' in key):
+                warn(f"couldn't determine corresponding lecture for trace key "
+                     f"'{key}'. Trace will be excluded from plot")
+        keys = [k for k in keys if k not in skip_keys]
+
+        to_plot = [self.get_lecture_traj(l) for l in lectures]
+        for q in questions:
+            if isinstance(q, str):
+                to_plot.append(self.get_question_vecs(lectures=q))
+            else:
+                to_plot.append(self.get_question_vecs(qids=q))
+
+        for p in participants:
+            if isinstance(p, int):
+                p = f"P{p}"
+            if isinstance(p, str):
+                p = self.participants[self.participants == p][0]
+            for key in keys:
+                lec = 'forces' if 'forces' in key else 'bos'
+                lec_traj = self.get_lecture_traj(lec)
+                trace = p.get_trace(key)
+                # weight each timepoint of lecture by knowledge
+                to_plot.append(lec_traj * trace[:, np.newaxis])
+
+        return hyp.plot(to_plot, **kwargs)
+
+    ##########################################
+    #              DATA LOADERS              #
+    ##########################################
+    def load_participants(self, load_avg=False):
+        participants = []
+        for pid in range(1, N_PARTICIPANTS + 1):
+            path = opj(PARTICIPANTS_DIR, f'P{pid}.npy')
+            p = np.load(path, allow_pickle=True).item()
+            participants.append(p)
+        self.participants = np.array(participants)
+        if load_avg:
+            self.load_avg_participant()
+            return self.participants, self.avg_participant
+        return self.participants
+
+    def load_avg_participant(self):
+        path = opj(PARTICIPANTS_DIR, 'avg.npy')
+        self.avg_participant = np.load(path, allow_pickle=True).item()
+        return self.avg_participant
+
+    def save_participants(self, filepaths=None, allow_overwrite=False):
+        if filepaths is None:
+            filepaths = [None] * (len(self.participants) + 1)
+        if len(filepaths) != len(self.participants) + 1:
+            raise ValueError("`filepaths` should contain one path per "
+                             "participant (including average participant, last)")
+        for p, fpath in zip(list(self.participants) + [self.avg_participant], filepaths):
+            p.save(filepath=fpath, allow_overwrite=allow_overwrite)
+
+    def load_transcript(self, lecture, splitlines=False):
+        if isinstance(lecture, str):
+            if lecture not in ('forces', 'bos'):
+                raise ValueError("lecture may be one of: 'forces', 'bos'")
+            path = opj(RAWDIR, f'{lecture}_transcript_timestamped.txt')
+            with open(path, 'r') as f:
+                transcript = f.read()
+            if lecture == 'forces':
+                self.forces_transcript = transcript
+            else:
+                self.bos_transcript = transcript
+            if splitlines:
+                transcript = transcript.splitlines()
+        elif hasattr(lecture, '__iter__'):
+            transcript = []
+            for l in lecture:
+                transcript.append(self.load_transcript(l, splitlines=splitlines))
+        else:
+            raise ValueError("lecture should be either a str or an iterable of strs")
+        return transcript
+
+    def load_questions(self):
+        path = opj(RAWDIR, 'questions.tsv')
+        self.questions = pd.read_csv(
+            path,
+            sep='\t',
+            names=['index', 'lecture', 'question', 'A', 'B', 'C', 'D'],
+            index_col='index'
+        )
+        return self.questions
+
+    def load_windows(self, lecture):
+        if hasattr(lecture, '__iter__') and not isinstance(lecture, str):
+            windows = []
+            for l in lecture:
+                windows.append(self.load_windows(l))
+        elif lecture not in ('forces', 'bos'):
+            raise ValueError("lecture may be one of: 'forces', 'bos'")
+        else:
+            windows = np.load(opj(RAWDIR, f'{lecture}_windows.npy'))
+            if lecture == 'forces':
+                self.forces_windows = windows
+            else:
+                self.bos_windows = windows
+        return windows
+
+    def load_lecture_trajs(self):
+        self.forces_traj = np.load(opj(TRAJS_DIR, 'forces_lecture.npy'))
+        self.bos_traj = np.load(opj(TRAJS_DIR, 'bos_lecture.npy'))
+        return self.forces_traj, self.bos_traj
+
+    def load_question_vectors(self):
+        self.question_vectors = np.load(opj(TRAJS_DIR, 'all_questions.npy'))
+        return self.question_vectors
+
+    def load_answer_vectors(self):
+        self.answer_vectors = np.load(opj(TRAJS_DIR, 'all_answers.npy'))
+        return self.answer_vectors
+
+    def load_embeddings(self):
+        self.forces_embedding = np.load(opj(EMBS_DIR, 'forces_lecture.npy'))
+        self.bos_embedding = np.load(opj(EMBS_DIR, 'bos_lecture.npy'))
+        self.question_embeddings = np.load(opj(EMBS_DIR, 'questions.npy'))
+        return self.forces_embedding, self.bos_embedding, self.question_embeddings
+
+    def load_cv(self):
+        self.cv = np.load(opj(MODELS_DIR, 'fit_CV.npy'), allow_pickle=True).item()
+        return self.cv
+
+    def load_lda(self):
+        self.lda = np.load(opj(MODELS_DIR, 'fit_LDA.npy'), allow_pickle=True).item()
+        return self.lda
+
+    def load_umap(self):
+        self.umap_reducer = np.load(opj(MODELS_DIR, 'fit_UMAP.npy'), allow_pickle=True).item()
+        return self.umap_reducer
+
+    def load_wordle_mask(self, path=None):
+        if path is None:
+            path = opj(DATADIR, 'wordle-mask.jpg')
+        self.wordle_mask = np.array(open_image(path))
+        return self.wordle_mask
---
+++ code/khan_helpers/khan_helpers/participant.py
@@ -0,0 +1,228 @@
+import numpy as np
+import pandas as pd
+from ast import literal_eval
+from html import unescape
+from os.path import isfile, join as opj
+from scipy.spatial.distance import cdist
+from scipy.stats import entropy
+from experiment import PARTICIPANTS_DIR, RAWDIR
+from helpers import symmetric_kl
+
+class Participant:
+    def __init__(self, subid, data=None, raw_data=None, date_collected=None):
+        self.subID = subid
+        self.data = data
+        _fallback_msg = "Attribute only set for participant created from raw " \
+                       "PsiTurk data"
+        if raw_data is None:
+            self.raw_data = _fallback_msg
+        else:
+            self.raw_data = raw_data
+        if date_collected is None:
+            self.date_collected = _fallback_msg
+        else:
+            self.date_collected = date_collected
+        self.traces = {}
+        self.maps = {}
+
+    @classmethod
+    def from_psiturk(cls, psiturk_data, subid):
+        raw_data = literal_eval(psiturk_data['datastring'])
+        date_collected = psiturk_data['beginhit'].split()[0]
+        p = cls(subid=subid, raw_data=raw_data, date_collected=date_collected)
+        p.data = p._grade()
+        return p
+
+    @property
+    def all_questions(self, questions_path=None):
+        if questions_path is None:
+            questions_path = opj(RAWDIR, 'questions.tsv')
+        return pd.read_csv(questions_path,
+                           sep='\t',
+                           names=['index', 'video', 'question',
+                                  'A', 'B', 'C', 'D'],
+                           index_col='index')
+
+    def __str__(self):
+        return self.subID
+
+    def __eq__(self, other):
+        return self.subID == other
+
+    def _grade(self):
+        # grades raw data to set self.data
+        data = []
+        question_blocks = (3, 8, 13)
+        all_qs = self.all_questions
+        for set_num, qblock in enumerate(question_blocks):
+            question_block = self.raw_data['data'][qblock]['trialdata']
+            answer_block = literal_eval(
+                self.raw_data['data'][qblock + 1]['trialdata']['responses']
+            )
+            for q_num, q in enumerate(question_block):
+                # deal with HTML characters
+                q_text = unescape(q['prompt']).replace(chr(8217), "'")
+                # match question text to question in dataset
+                q_info = all_qs.loc[all_qs.question == q_text]
+                # error on failure to match questions
+                if not q_info.values.any():
+                    raise KeyError(f"failed to find question matching {q_text}")
+                # set qID and reference lecture
+                qid = q_info.index[0]
+                lec = q_info.loc[qid, 'video']
+                # get answer
+                ans_text = unescape(answer_block[f'Q{q_num}']).replace(chr(8217), "'")
+                # error on failure to match question or unexpected letter
+                try:
+                    ans_let = q_info.squeeze()[q_info.squeeze() == ans_text].index[0]
+                except IndexError as e:
+                    raise KeyError(
+                        f"failed to find answer matching {ans_text}"
+                    ) from e
+                assert ans_let in 'ABCD', f"found answer code {ans_let}, " \
+                                          f"expected one of: 'A', 'B', 'C', 'D'"
+                # set accuracy for response
+                acc = 1 if ans_let == 'A' else 0
+                data.append([qid, acc, ans_let, set_num, lec])
+        return pd.DataFrame(data, columns=['qID', 'accuracy', 'response', 'qset', 'lecture'])
+
+    def _repr_html_(self):
+        # for displaying in Jupyter (IPython) notebooks
+        print(self.__str__())
+        try:
+            return self.data.to_html()
+        except AttributeError:
+            return "Individual data unavailable"
+
+    def head(self, *args, **kwargs):
+        print(self.__str__())
+        return self.data.head(*args, **kwargs)
+
+    def get_data(self, qset=None, lecture=None):
+        # return (a subset of) the subject's data
+        if self.data is None:
+            return f"No data for participant: {self.subID}"
+
+        lec_keys = {'general': 0, 'forces': 1, 'bos': 2}
+        if isinstance(qset, int):
+            qset = [qset]
+        # funnel ints, strings, lists of either into list of ints
+        if isinstance(lecture, int):
+            lecture = [lecture]
+        elif isinstance(lecture, str):
+            lecture = [lec_keys[lecture]]
+        elif hasattr(lecture, '__iter__'):
+            for l in range(len(lecture)):
+                if isinstance(lecture[l], str):
+                    lecture[l] = lec_keys[lecture[l]]
+
+        d = self.data
+        if qset is not None:
+            d = d.loc[d['qset'].isin(qset)]
+        if lecture is not None:
+            d = d.loc[d['lecture'].isin(lecture)]
+        return d
+
+    def get_trace(self, trace_key):
+        """
+        Getter for self.traces
+        :param trace_key: str
+                The key for the trace to be returned
+        :return: trace: np.ndarray
+                The trace stored under the given `trace_key`
+        """
+        try:
+            return self.traces[trace_key]
+        except KeyError as e:
+            raise KeyError(
+                f"No trace stored for {self} under {trace_key}. "
+                f"Stored traces are: {', '.join(self.traces.keys())}"
+            ) from e
+
+    def reconstruct_trace(
+            self,
+            exp,
+            content=None,
+            lecture=None,
+            qset=None,
+            store=None,
+            recon_lec=None
+    ):
+        """
+        Reconstructs a participant's knowledge trace based on a lecture's
+        trajectory, a set of questions' topic vectors, and binary accuracy scores
+        :param exp: Experiment object
+                Used to access lecture trajectory and question topic vectors
+        :param content: numpy.ndarray
+                The content against which to weight knowledge, as derived by
+                question accuracy
+        :param lecture: str, int, or iterable of strs/ints
+                The lecture(s) for which to get questions and scores. If
+                [default] None, get questions and scores for both lectures
+                (& general knowledge)
+        :param qset: int or iterable of ints
+                The question set for which to get questions and scores. If
+                [default] None, get data for all question sets
+        :param store: str or None
+                The key under which the reconstructed trace should be stored in
+                self.traces. If [default] None, don't store the trace
+        :param recon_lec: int or str
+                The lecture trajectory to use in reconstructing the trace (if
+                not passed directly via content). Useful if passing an iterable
+                to lecture in order to get questions related to multiple lectures
+        :return trace: numpy.ndarray
+                The reconstructed trace, describing "knowledge" for each
+                timepoint of given content.
+        """
+        data = self.get_data(qset=qset, lecture=lecture)
+        acc = data['accuracy'].astype(bool)
+        qids = data['qID'].tolist()
+        question_vecs = exp.get_question_vecs(qids=qids)
+        if content is None:
+            if isinstance(lecture, (int, str)):
+                content = exp.get_lecture_traj(lecture)
+            elif hasattr(lecture, '__iter__'):
+                if recon_lec is not None:
+                    content = exp.get_lecture_traj(recon_lec)
+                else:
+                    raise ValueError("Must specify `content` or `recon_lec` if "
+                                     "passing multiple `lecture`s")
+            else:
+                raise ValueError(
+                    "lecture should be one of: str, int, iterable of str/int"
+                )
+        # compute timepoints by questions weights matrix
+        wz = 1 - cdist(content, question_vecs, metric=symmetric_kl)
+        # normalize
+        wz -= wz.min()
+        wz /= wz.max()
+        # sum over questions (total possible weights for each timepoint)
+        a = wz.sum(axis=1)
+        # sum weights from correctly answered questions at each timepoint
+        b = wz[:, acc].sum(axis=1)
+        # divide weight from correct answers by total weight
+        trace = b / a
+        # store trace in object
+        if store is not None:
+            self.store_trace(trace=trace, store_key=store)
+        return trace
+
+    def save(self, filepath=None, allow_overwrite=False):
+        if filepath is None:
+            filepath = opj(PARTICIPANTS_DIR, f'{self.subID}.npy')
+        if not allow_overwrite and isfile(filepath):
+            print(f"{self.subID} not saved because {filepath} already exists. "
+                  "Set allow_overwrite to True to replace the existing file")
+        else:
+            np.save(filepath, self)
+
+    def store_trace(self, trace, store_key):
+        self.traces[store_key] = trace
+
+    # def plot(self, keys, **kwargs):
+    #     # wraps hypertools.plot for plotting multiple reconstructed traces
+    #     # (see Experiment.plot for multisubject plotting)
+    #     if isinstance(keys, str):
+    #         keys = [keys]
+    #     traces_toplot = [self.traces[k] for k in keys]
+    #     return hyp.plot(traces_toplot, **kwargs)
