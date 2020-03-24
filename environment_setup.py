dc9a69284bf07ca48d56b2e992aaeb75b33f2302
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-03-23 22:38:01 -0400

    python script for configuring docker container

---
+++ environment_setup.py
@@ -0,0 +1,53 @@
+from shlex import split as split_command
+from subprocess import PIPE, Popen, SubprocessError, TimeoutExpered
+from sys import exc_info, exit
+
+
+def run_with_live_output(command):
+    """
+    runs a shell command via subprocess.Popen,
+    avoids Python's stdout  buffering to show
+    real-time output
+    """
+    cmd = split_command(command)
+    try:
+        process = Popen(cmd, stdout=PIPE)
+        # poll returns None when process terminates
+        while process.poll() is None:
+            output = process.stdout.readline()
+            if output:
+                print(output.strip().decode())
+    except TimeoutExpered as e:
+        # shouldn't occur, but handle it just to be safe
+        # so we can clean up child processes
+        process.kill()
+        raise SubprocessError(f"Child process timed out while running "
+                              "{command}") from e
+    except SubprocessError as e:
+        raise SubprocessError(f"ERROR {exc_info()[1]} occurred while running "
+                               "{command} ") from e
+    # return the completed process's return code
+    return process.poll()
+
+
+commands = [
+    # update setuptools
+    "conda update -y setuptools",
+    # install jupyterlab
+    "conda install -yc conda-forge jupyterlab",
+    # install proper package versions (use pip to avoid
+    # dreadfully slow conda compatibility checks)
+    "pip install -r analyses-requirements.txt",
+    # install js & css files for some convenient jupyter notebook extensions
+    "jupyter contrib nbextion install --user",
+    # download nltk stop-words corpus
+    # (have to run in subprocess because package wasn't installed when this
+    # interpreter was loaded)
+    '''python -c "import nltk; nltk.download('stopwords')"'''
+]
+
+for i, command in enumerate(commands):
+    print(f"Running step {i + 1}/{len(commands)}: {command}")
+    return_code = run_with_live_output(command)
+    if return_code != 0:
+        exit(return_code)
