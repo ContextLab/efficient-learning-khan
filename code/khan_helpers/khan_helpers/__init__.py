275e8c923006bd1a0d5f02b33c1b608a59164284
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-13 12:06:25 -0400

    switched to setup.cfg format

--- code/khan_helpers/khan_helpers/__init__.py
+++ code/khan_helpers/khan_helpers/__init__.py
@@ -1,18 +1,16 @@
-from os.path import dirname, realpath
-from pkg_resources import get_distribution
+from pathlib import Path
 from IPython.display import display, Markdown
-
 from .experiment import Experiment
 from .participant import Participant
 
 
-# see version number in setup.py
-__version__ = get_distribution("khan_helpers").version
+version_info = (0, 0, 1)
+__version__ = '.'.join(map(str, version_info))
 
 github_link = "https://github.com/contextlab/efficient-learning-khan/tree/master/code/khan_helpers"
-pkg_dir = dirname(dirname(realpath(__file__)))
+pkg_dir = Path(__file__).resolve().parents[1]
 message = Markdown(
-    "Experiment & Paricipant classes, helper functions, and variables used "
+    "Experiment & Participant classes, helper functions, and variables used "
     f"across multiple notebooks can be found in `{pkg_dir}`, or on GitHub, "
     f"[here]({github_link}).<br />You can also view source code directly from "
     "the notebook with:<br /><pre>    from khan_helpers.helpers import "
--- code/khan_helpers/setup.py
+++ code/khan_helpers/setup.py
@@ -1,24 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-NAME = "khan_helpers"
-VERSION = "0.0.1"
-AUTHOR = "Paxton Fitzpatrick, Contextual Dynamics Lab"
-AUTHOR_EMAIL = "contextualdynamics@gmail.com"
-URL = "https://github.com/ContextLab/efficient-learning-khan"
-DOWNLOAD_URL = URL
-LICENSE = "MIT"
-PYTHON_REQUIRES = ">=3.6"
-DESCRIPTION = "Helper classes and functions for reproducing the analyses"
-
-setup(
-    name=NAME,
-    version=VERSION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    url=URL,
-    download_url=DOWNLOAD_URL,
-    license=LICENSE,
-    python_requires=PYTHON_REQUIRES,
-    description=DESCRIPTION
-)
+# reads from setup.cfg
+setup()
