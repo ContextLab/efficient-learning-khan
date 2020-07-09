2c19deff0406df9647ace8c551398b066320e388
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-07-09 19:55:46 -0400

    use pathlib, minor cleanup

--- code/khan_helpers/khan_helpers/helpers.py
+++ code/khan_helpers/khan_helpers/helpers.py
@@ -2,7 +2,7 @@ import re
 import numpy as np
 from datetime import timedelta
 from inspect import getsource
-from os.path import join as opj
+from pathlib import Path
 from IPython.display import HTML
 from IPython.core.oinspect import pylight
 from nltk.corpus import stopwords
@@ -13,13 +13,13 @@ from scipy.stats import entropy
 ##########################################
 #            REUSED VARIABLES            #
 ##########################################
-DATA_DIR = '/mnt/data'
-RAW_DIR = opj(DATA_DIR, 'raw')
-PARTICIPANTS_DIR = opj(DATA_DIR, 'participants')
-TRAJS_DIR = opj(DATA_DIR, 'trajectories')
-EMBS_DIR = opj(DATA_DIR, 'embeddings')
-MODELS_DIR = opj(DATA_DIR, 'models')
-FIG_DIR = '/mnt/paper/figs/'
+DATA_DIR = Path('/mnt/data')
+RAW_DIR = DATA_DIR.joinpath('raw')
+PARTICIPANTS_DIR = DATA_DIR.joinpath('participants')
+TRAJS_DIR = DATA_DIR.joinpath('trajectories')
+EMBS_DIR = DATA_DIR.joinpath('embeddings')
+MODELS_DIR = DATA_DIR.joinpath('models')
+FIG_DIR = Path('/mnt/paper/figs/')
 N_PARTICIPANTS = 50
 LECTURE_WSIZE = 20
 STOP_WORDS = stopwords.words('english') + ["even", "i'll", "i'm", "let", "let's",
@@ -33,26 +33,27 @@ STOP_WORDS = stopwords.words('english') + ["even", "i'll", "i'm", "let", "let's"
 def _ts_to_sec(ts):
     # converts timestamp of elapsed time from "MM:SS" format to scalar
     mins, secs = ts.split(':')
-    mins, secs = int(mins), int(secs)
-    return timedelta(minutes=mins, seconds=secs).total_seconds()
+    return timedelta(minutes=int(mins), seconds=int(secs)).total_seconds()
 
 
 def corr_mean(rs, axis=0):
     # computes the mean of correlation coefficients, performing the
-    # Fisher z-transformation & inverse z-transormation before & after
+    # Fisher z-transformation & inverse z-transformation before & after
     return z2r(np.nanmean([r2z(r) for r in rs], axis=axis))
 
 
 def format_text(textlist, sw=STOP_WORDS):
     # some simple text preprocessing:
-    # removes punctuation & symbols, converts to all lowercase,
-    # splits hyphenated words, removes apostrophes
+    # - removes punctuation & symbols
+    # - normalizes everything to lowercase,
+    # - splits hyphenated words, removes apostrophes
     clean_textlist = []
     for chunk in textlist:
         no_punc = re.sub("[^a-zA-Z\s'-]+", '', chunk.lower()).replace('-', ' ')
         no_stop = ' '.join([word for word in no_punc.split() if word not in sw])
         clean_text = re.sub("'+", '', no_stop)
         clean_textlist.append(clean_text)
+
     return clean_textlist
 
 
@@ -101,7 +102,7 @@ def show_source(obj):
     try:
         return HTML(pylight(src))
     except AttributeError:
-        # pylight doesn't work on certain types
+        # pylight just doesn't work on certain types
         return src
 
 
