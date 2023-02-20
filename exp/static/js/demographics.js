2ddc5d0b85db0ca71ba1ef71e1735a4092a42802
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2023-02-20 14:32:07 -0500

    lock google form, fill consent form & debriefing with template text

--- exp/static/js/demographics.js
+++ exp/static/js/demographics.js
@@ -1,6 +1,6 @@
 var runDemographics = function(options) {
   window.onload = function() {
-    $("body").append("<div id='googleform'><iframe src='https://docs.google.com/forms/d/e/1FAIpQLSfBL9Q7UNQg9q8lbO-TkKJpdaCS4thMr3x-MjpC_q0xOfA77w/viewform?embedded=true' width='" +
+    $("body").append("<div id='googleform'><iframe src='https://docs.google.com/forms/d/e/1FAIpQLSfsAsQ1NlKa26G3Q2EwI6gq_kW599LhMkwCa7Q9XCJLB7Ow2A/viewform?embedded=true' width='" +
         $(window).width() + "' height='" +
         $(window).height() + "' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe></div>")
 
